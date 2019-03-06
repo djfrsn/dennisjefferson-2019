519c0f3c887f1e7007bb86efa4413946f57b5462
Dennis Jefferson <dennismjefferson@gmail.com>
2019-03-05 19:19:17 -0800

    First implementation of file post from FE

--- Photography Website/lib/front-end/uploadPhotos.js
+++ Photography Website/lib/front-end/uploadPhotos.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
 
-async function uploadPhotos(options) {
-  console.log('lib/uploadPhoto', options);
-  const res = await axios.post('/photos/upload', { foo: 'bar' });
+function appendFilesToFormData(formData, acceptedFiles) {
+  acceptedFiles.forEach((file, i) => formData.append(i, file, file.name));
+
+  return formData;
+}
+
+async function uploadPhotos({ acceptedFiles, rejectedFiles }) {
+  console.log('lib/uploadPhoto', acceptedFiles);
+
+  let photoData = appendFilesToFormData(new FormData(), acceptedFiles);
+
+  const res = await axios.post('/photos/upload', photoData, {
+    headers: {
+      accept: 'application/json',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Content-Type': `multipart/form-data; boundary=${photoData._boundary}`
+    }
+  });
   console.log('uploadPhoto/res ', res);
 }
 
